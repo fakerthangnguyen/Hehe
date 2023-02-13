ff8761ae3782d08e0c1a87c9a8612e8917c282ca
fakerthangnguyen <112310851+fakerthangnguyen@users.noreply.github.com>
2023-02-13 23:59:41 +0700

    Add files via upload
    create

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Gạo';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 14-2';
+const titleCard = 'Tặng bé Gạo';
+const contentCard = 'Chúc Gạo 14-2 zui zẻ nhen,rảnh rảnh a ngồi làm chơi,a cũng k biết nói gì cả,chỉ chúc e z hoy :))))))))))';
+
+
+const giftImage = '327108524_721176936182826_1609116791398530153_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
